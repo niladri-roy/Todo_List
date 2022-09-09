5515c6d697e49e3519e1ffa74254f48c863128d1
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2022-09-09 09:22:34 +0530

    ongoing

--- script.js
+++
@@ -1,9 +0,0 @@
-function addItem(event){
-    event.preventDefault();
-    let text = document.getElementById("todo-input");
-
-    db.collection("todo-items").add({
-        text: text.value,
-        status: "active"
-    })
-}
